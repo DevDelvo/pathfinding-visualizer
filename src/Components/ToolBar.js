e69c8f79876a4a7f24ebdf0f474b8bf8530eea18
DevDelvo <kevinbdelvo@gmail.com>
2019-10-09 11:38:52 -0400

    refactored grid rendering

---
+++ src/Components/ToolBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Toolbar extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>Toolbar</div>
+        )
+    }
+}
--- src/PathFindingVisualizer/PathfindingVisualizer.js
+++ src/PathFindingVisualizer/PathfindingVisualizer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Node from './Node/Node';
+// import { createNode, createBoard, getNewGridWithWallToggled } from './createBoardHelper'
 
 import './PathfindingVisualizer.css'
 
@@ -8,30 +9,71 @@ export default class PathfindingVisualizer extends Component {
         super(props);
         this.state = {
             nodes: [],
+            startNode: {
+                row: 10, col: 15
+            },
+            endNode: {
+                row: 10, col: 35
+            },
+            mouseIsPressed: false,
         };
     }
 
     componentDidMount() {
-        const nodes = this.renderNodes();
+        const nodes = this.createBoard();
         this.setState({ nodes });
     }
 
-    renderNodes = () => {
+    handleMouseDown(row, col) {
+        const newGrid = this.getNewGridWithWallToggled(this.state.grid, row, col);
+        this.setState({ grid: newGrid, mouseIsPressed: true });
+    }
+
+    handleMouseEnter(row, col) {
+        if (!this.state.mouseIsPressed) return;
+        const newGrid = this.getNewGridWithWallToggled(this.state.grid, row, col);
+        this.setState({ grid: newGrid });
+    }
+
+    handleMouseUp() {
+        this.setState({ mouseIsPressed: false })
+    }
+
+    createNode = (col, row) => {
+        const { startNode, endNode } = this.state;
+        return {
+            col,
+            row,
+            start: row === startNode.row && col === startNode.col,
+            end: row === endNode.row && col === endNode.col,
+            distance: Infinity,
+            isVisited: false,
+            previousNode: null,
+        };
+    }
+
+    createBoard = () => {
         const nodes = [];
         for (let row = 0; row < 20; row++) {
             const currRow = [];
-            for (let col = 0; col < 50; col++) {
-                const currentNode = {
-                    row,
-                    col,
-                    start: row === 10 && col === 5,
-                    end: row === 10 && col === 45,
-                };
-                currRow.push(currentNode);
+            for (let col = 0; col < 40; col++) {
+                const newNode = this.createNode(col, row);
+                currRow.push(newNode);
             }
             nodes.push(currRow);
         }
         return nodes;
+    }
+
+    getNewGridWithWallToggled = (grid, row, col) => {
+        const newGrid = grid.slice();
+        const node = newGrid[row][col];
+        const newNode = {
+            ...node,
+            isWall: !node.isWall,
+        };
+        newGrid[row][col] = newNode;
+        return newGrid;
     }
 
     render() {
